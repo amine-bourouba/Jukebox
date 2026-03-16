b94dcbea6447a8ac9e2fdc91a6cec2668332e797
amine bourouba <aminebourouba9@gmail.com>
2026-03-16 16:00:51 +0100

    [JKX] FEAT: Add vitest configuration

---
+++ backend/vitest.config.ts
@@ -0,0 +1,16 @@
+import swc from 'unplugin-swc';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'node',
+    include: ['src/**/*.spec.ts'],
+  },
+  plugins: [
+    swc.vite({
+      module: { type: 'es6' },
+      tsconfigFile: './tsconfig.app.json',
+    }),
+  ],
+});
