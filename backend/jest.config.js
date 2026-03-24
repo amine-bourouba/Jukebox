d228fff9d457dc840f5fe491fff577e032d1ad78
amine bourouba <aminebourouba9@gmail.com>
2026-03-24 14:44:57 +0100

    [JKX] FIX: Handle tseting configuration bug

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,15 +1,16 @@
 /* eslint-disable */
-import { readFileSync } from 'fs';
+const { readFileSync } = require('fs');
+const path = require('path');
 
 // Reading the SWC compilation config for the spec files
 const swcJestConfig = JSON.parse(
-  readFileSync(`${__dirname}/.spec.swcrc`, 'utf-8')
+  readFileSync(path.join(__dirname, '.spec.swcrc'), 'utf-8')
 );
 
 // Disable .swcrc look-up by SWC core because we're passing in swcJestConfig ourselves
 swcJestConfig.swcrc = false;
 
-export default {
+module.exports = {
   displayName: '@jukebox/backend',
   preset: '../jest.preset.js',
   testEnvironment: 'node',
